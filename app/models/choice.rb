09557bbd3082fe13035762ed5a5fc99622b4c8a9
wirate <wiratesltd@gmail.com>
2012-09-26 22:30:24 +0530

    rails quiz

---
+++ app/models/choice.rb
@@ -0,0 +1,9 @@
+# ITESM CEM, May 11, 2011.
+# Ruby Source File
+# Activity::  Final Project: Design Patterns Quiz App
+# Author::    1165536 Juliana Pena
+
+class Choice < ActiveRecord::Base
+  belongs_to :question
+  has_many :users
+end
